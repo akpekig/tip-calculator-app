8a8584a1540d87775b459fa3fc9dcb4ccf35492b
akpekig <gillianakpeki@hotmail.co.uk>
2023-06-01 22:13:58 +0100

    Add main page

--- README.md
+++ README.md
@@ -8,11 +8,9 @@ This is a solution to the [Tip calculator app challenge on Frontend Mentor](http
   - [The challenge](#the-challenge)
   - [Screenshot](#screenshot)
   - [Links](#links)
-  <!-- - [My process](#my-process)
+  - [My process](#my-process)
   - [Built with](#built-with)
   - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources) -->
 - [Author](#author)
 - [Acknowledgments](#acknowledgments)
 
@@ -26,16 +24,16 @@ Users should be able to:
 - See hover states for all interactive elements on the page
 - Calculate the correct tip and total cost of the bill per person
 
-<!-- ### Screenshot
+### Screenshot
 
-![](./screenshot.jpg) -->
+![](./screenshot.png)
 
 ### Links
 
 - Solution URL: [https://github.com/akpekig/tip-calculator-app](https://github.com/akpekig/tip-calculator-app)
 - Live Site URL: [https://akpekig.github.io/tip-calculator-app/](https://akpekig.github.io/tip-calculator-app/)
 
-<!-- ## My process
+## My process
 
 ### Built with
 
@@ -45,40 +43,18 @@ Users should be able to:
 - CSS Grid
 - Mobile-first workflow
 - [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+- [Gatsby](https://www.gatsbyjs.com/) - React framework
+- [Tailwind CSS](https://tailwindcss.com/) - For styles
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+Preventing infinity values in JS.
 
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
 ```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
+if (perPerson > Number.MAX_SAFE_INTEGER) {
+  ...
 }
 ```
-
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-
-### Continued development
-
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
-
-### Useful resources
-
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept. -->
 
 ## Author
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,78 +9,131 @@ import Result from "@components/result";
 import Person from "@src/images/icon-person.inline.svg";
 import Dollar from "@src/images/icon-dollar.inline.svg";
 
-const tips = [5, 10, 15, 25, 50];
-const results = [
-  {
-    title: "Tip Amount",
-    value: 0,
-  },
-  {
-    title: "Total",
-    value: 0,
-  },
-];
-
-const IndexPage = () => (
-  <Layout>
-    <form className="grid w-full gap-8 desktop:grid-cols-2 desktop:grid-rows-5 desktop:pl-5">
-      <NumberInput
-        Icon={Dollar}
-        inputId={"bill"}
-        inputType={"text"}
-        title={"Bill"}
-        placeholder={"0"}
-      />
-      <fieldset className="inline-flex h-fit flex-col flex-nowrap desktop:row-start-2 desktop:row-end-4 desktop:mt-4">
-        <legend className="text-xs text-neutral-500">Select Tip %</legend>
-        <div className="my-[0.625rem] inline-flex w-full flex-wrap gap-4 text-neutral-100 desktop:my-3">
-          {tips.map((item, index) => (
-            <RadioInput
-              key={`tip${item}`}
-              inputId={`tip${index}`}
+const IndexPage = () => {
+  const [bill, setBill] = React.useState(0);
+  const [tip, setTip] = React.useState(0);
+  const [people, setPeople] = React.useState(0);
+  const [tipResult, setTipResult] = React.useState(0);
+  const [totalResult, setTotalResult] = React.useState(0);
+  const [error, setError] = React.useState();
+  const formRef = React.useRef();
+  const tips = [5, 10, 15, 25, 50];
+  const results = [
+    {
+      title: "Tip Amount",
+      value: tipResult,
+    },
+    {
+      title: "Total",
+      value: totalResult,
+    },
+  ];
+  const handleInput = (event, fn, value, errorMessage) => {
+    if (errorMessage && parseInt(event.target.value) === 0) {
+      setError(errorMessage);
+      return false;
+    } else if (error) {
+      setError(false);
+    } else if (!isNaN(event.target.value)) {
+      fn(event.target.value);
+    } else {
+      event.target.value = value;
+    }
+  };
+  const handleClick = () => {
+    setBill(0);
+    setTip(0);
+    setPeople(0);
+    setTipResult(0);
+    setTotalResult(0);
+    formRef.current.reset();
+  };
+  React.useEffect(() => {
+    /**
+     * Calculate total and tips per person.
+     */
+    if (people > 0 && bill > 0) {
+      const perPerson = bill / people;
+      if (perPerson > Number.MAX_SAFE_INTEGER) {
+        setTotalResult("Too large!");
+        setTipResult("Too large!");
+      } else {
+        setTotalResult(perPerson);
+        if (tip > 0 && tip <= 100) {
+          setTipResult((tip / 100) * perPerson);
+        }
+      }
+    }
+  }, [bill, tip, people]);
+  return (
+    <Layout>
+      <form
+        className="grid w-full gap-8 desktop:grid-cols-2 desktop:grid-rows-5 desktop:pl-5 desktop:[&>fieldset+label]:mt-8"
+        ref={formRef}
+      >
+        <NumberInput
+          Icon={Dollar}
+          inputId={"bill"}
+          inputType={"text"}
+          title={"Bill"}
+          placeholder={"0"}
+          fn={e => handleInput(e, setBill, bill)}
+        />
+        <fieldset className="inline-flex h-fit flex-col flex-nowrap desktop:row-start-2 desktop:row-end-4 desktop:mt-4">
+          <legend className="text-xs text-neutral-500">Select Tip %</legend>
+          <div className="my-[0.625rem] inline-flex w-full flex-wrap gap-4 text-neutral-100 desktop:my-3">
+            {tips.map((item, index) => (
+              <RadioInput
+                key={`tip${item}`}
+                inputId={`tip${index}`}
+                inputName={"tip"}
+                value={item}
+                fn={e => handleInput(e, setTip, tip)}
+              />
+            ))}
+            <NumberInput
+              inputId={"customTip"}
+              inputType={"custom"}
               inputName={"tip"}
-              value={item}
+              title={"Custom"}
+              fn={e => handleInput(e, setTip, tip)}
+            />
+          </div>
+        </fieldset>
+        <NumberInput
+          Icon={Person}
+          inputId={"people"}
+          inputType={"text"}
+          title={"Number of People"}
+          placeholder={"0"}
+          error={error}
+          fn={e => handleInput(e, setPeople, people, "Can't be zero")}
+        />
+        <section
+          className="flex flex-col flex-nowrap justify-evenly gap-7 rounded-2xl bg-neutral-600 px-10 py-10 desktop:col-start-2 desktop:row-span-full desktop:gap-10"
+          aria-label="Result"
+        >
+          {results.map((item, index) => (
+            <Result
+              key={`result${index}`}
+              title={item.title}
+              value={item.value}
             />
           ))}
-          <NumberInput
-            inputId={"customTip"}
-            inputType={"custom"}
-            inputName={"tip"}
-            title={"Custom"}
-          />
-        </div>
-      </fieldset>
-      <NumberInput
-        Icon={Person}
-        inputId={"people"}
-        inputType={"text"}
-        title={"Number of People"}
-        placeholder={"0"}
-        integer={true}
-        extraClassName={"desktop:mt-8"}
-      />
-      <section
-        className="flex flex-col flex-nowrap justify-evenly gap-7 rounded-2xl bg-neutral-600 px-10 py-10 desktop:col-start-2 desktop:row-span-full desktop:gap-10"
-        aria-label="Result"
-      >
-        {results.map((item, index) => (
-          <Result
-            key={`result${index}`}
-            title={item.title}
-            value={item.value}
-          />
-        ))}
-        <button
-          type="reset"
-          className="w-auto select-none rounded-md bg-primary p-2 text-base uppercase text-neutral-600 hover:bg-secondary disabled:pointer-events-none disabled:text-opacity-50 disabled:opacity-20 desktop:mt-auto"
-          aria-label="Reset"
-        >
-          Reset
-        </button>
-      </section>
-    </form>
-  </Layout>
-);
+          <button
+            type="reset"
+            className="w-auto select-none rounded-md bg-primary p-2 text-base uppercase text-neutral-600 duration-300 ease-in-out hover:bg-secondary disabled:pointer-events-none disabled:text-opacity-50 disabled:opacity-20 desktop:mt-auto"
+            disabled={!(bill || tip || people)}
+            aria-label="Reset"
+            onClick={handleClick}
+          >
+            Reset
+          </button>
+        </section>
+      </form>
+    </Layout>
+  );
+};
 
 export const Head = () => <Seo title="Home" />;
 
